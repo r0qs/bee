85c6b3436718eb7b3f7e55ad494cee192b135dd0
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2021-01-13 17:15:41 +0100

    Skip all errors

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -135,7 +135,7 @@ func (s *Service) RetrieveChunk(ctx context.Context, addr swarm.Address) (swarm.
 						}
 
 						select {
-						case resultC <- retrieveChunkResult{err: storage.ErrNotFound}:
+						case resultC <- retrieveChunkResult{err: err}:
 						default:
 						}
 						return
@@ -157,11 +157,7 @@ func (s *Service) RetrieveChunk(ctx context.Context, addr swarm.Address) (swarm.
 				peersResults++
 
 				if result.err != nil {
-					if errors.Is(result.err, storage.ErrNotFound) && peersResults != maxPeers {
-						break
-					}
-
-					return nil, result.err
+					break
 				}
 
 				if result.chunk == nil {
@@ -172,6 +168,12 @@ func (s *Service) RetrieveChunk(ctx context.Context, addr swarm.Address) (swarm.
 				return result.chunk, nil
 			case <-ctx.Done():
 				logger.Tracef("retrieval: failed to get chunk %s: %v", addr, ctx.Err())
+				return nil, storage.ErrNotFound
+			}
+
+			// all results received
+			if peersResults >= maxPeers {
+				logger.Tracef("retrieval: failed to get chunk %s", addr)
 				return nil, storage.ErrNotFound
 			}
 		}
