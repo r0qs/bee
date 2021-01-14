93c3980860b7af4509cd2812b0c879325fbf91dc
Nemanja Zbiljić <nemanja.zbiljic@gmail.com>
2021-01-14 13:37:15 +0100

    Lint fix

--- pkg/retrieval/retrieval.go
+++ pkg/retrieval/retrieval.go
@@ -126,7 +126,6 @@ func (s *Service) RetrieveChunk(ctx context.Context, addr swarm.Address) (swarm.
 			case <-ticker.C:
 				// break
 			case chunk := <-resultC:
-				peersResults++
 				return chunk, nil
 			case <-errC:
 				peersResults++
